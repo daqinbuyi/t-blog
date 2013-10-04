73b82e340e54521a623cfa09c8900f1f3acd7cf7
Justfly <sdpengfei1018@gmail.com>
2013-10-04 14:35:25 +0800

    pep8

--- uimodules.py
+++ uimodules.py
@@ -1,6 +1,7 @@
 import tornado.web
 from config import site_options
 
+
 class BaseModule(tornado.web.UIModule):
     def render(self):
         raise Exception("Not implement!")
@@ -8,6 +9,7 @@ class BaseModule(tornado.web.UIModule):
     def render_string(self, template_name, **kwargs):
         template_name = site_options["theme"] + '/' + template_name
         return super(BaseModule, self).render_string(template_name, **kwargs)
+
 
 class Article(BaseModule):
     def render(self, article, show_all=False):
@@ -26,36 +28,27 @@ class Pagination(BaseModule):
         else:
             page_size = site_options["index_page_size"]
         next = current_page + 1 if page_size * current_page < total else False
-        return self.render_string("module/pagination.html", prev=prev, next=next)
+        return self.render_string(
+            "module/pagination.html", prev=prev, next=next)
 
 
 class TagCloud(BaseModule):
     def render(self, tags):
-        return self.render_string(
-            "module/tag_cloud.html",
-            tags=tags
-        )
+        return self.render_string("module/tag_cloud.html", tags=tags)
 
 
 class BlogRoll(BaseModule):
     def render(self, links):
-        return self.render_string(
-            "module/blog_roll.html",
-            links=links
-        )
+        return self.render_string("module/blog_roll.html", links=links)
 
 
 class RecentPosts(BaseModule):
     def render(self, recent_posts):
         return self.render_string(
-            "module/recent_posts.html",
-            recent_posts=recent_posts
-        )
+            "module/recent_posts.html", recent_posts=recent_posts)
 
 
 class GoogleAnalytics(BaseModule):
     def render(self):
         return self.render_string(
-            "module/google_analytics.html",
-            google_analytics={}
-        )
+            "module/google_analytics.html", google_analytics={})
